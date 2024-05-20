09a0353f3b6f96f11298ab40c4f30bc8384dc54e
Samuel Carlsson <samuel.carlsson@volumental.com>
2024-05-20 21:07:18 +0200

    polyline

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,19 @@
 use svg::Document;
-use svg::node::element::Path;
-use svg::node::element::path::{Data, Command, Position};
+use svg::node::element::Polyline;
 
 const A4_PORTRAIT: (i32, i32, i32, i32) = (0, 0, 210, 297);
 
-struct Canvas {
-    data: Data,
-}
-
-impl Canvas {
-    fn new() -> Canvas {
-        Canvas { data: Data::new() }
-    }
-    fn move_to(&mut self, x: i32, y: i32) {
-        self.data.append(Command::Move(Position::Absolute, (x, y).into()));
-    }
-    
-    fn line_to(&mut self, x: i32, y: i32) {
-        self.data.append(Command::Line(Position::Relative, (x, y).into()));
-    }
-    
-    fn close(&mut self) {
-        self.data.append(Command::Close);
-    }
-}
-
 fn main() {
-    let mut canvas = Canvas::new();
-    canvas.move_to(10, 10);
-    canvas.line_to(0, 50);
-    canvas.line_to(50, 0);
-    canvas.line_to(0, -50);
-    canvas.close();
-
-    let path = Path::new()
+    let polyline = Polyline::new()
+        .set("points", "10,10, 20,30, 40,30")
         .set("fill", "none")
         .set("stroke", "black")
-        .set("stroke-width", 3)
-        .set("d", canvas.data);
+        .set("stroke-width", 1);
+    
 
     let document = Document::new()
         .set("viewBox", A4_PORTRAIT)
-        .add(path);
+        .add(polyline);
 
     svg::save("image.svg", &document).unwrap();
 }
