666fd7e61da33d7a4bd51a618fa8ab955822fc8f
Samuel Carlsson <samuel.carlsson@volumental.com>
2024-05-20 14:14:06 +0200

    draw to A4

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use svg::Document;
 use svg::node::element::Path;
 use svg::node::element::path::Data;
 
+const A4_PORTRAIT: (i32, i32, i32, i32) = (0, 0, 210, 297);
+
 fn main() {
     let data = Data::new()
         .move_to((10, 10))
@@ -15,9 +17,9 @@ fn main() {
         .set("stroke", "black")
         .set("stroke-width", 3)
         .set("d", data);
-
+     
     let document = Document::new()
-        .set("viewBox", (0, 0, 70, 70))
+        .set("viewBox", A4_PORTRAIT)
         .add(path);
 
     svg::save("image.svg", &document).unwrap();
