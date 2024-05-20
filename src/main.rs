da57de8275b0c91fec61926254c4c834f0fdcd27
Samuel Carlsson <samuel.carlsson@volumental.com>
2024-05-20 20:44:40 +0200

    canvas struct

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,44 @@
 use svg::Document;
 use svg::node::element::Path;
-use svg::node::element::path::Data;
+use svg::node::element::path::{Data, Command, Position};
 
 const A4_PORTRAIT: (i32, i32, i32, i32) = (0, 0, 210, 297);
 
+struct Canvas {
+    data: Data,
+}
+
+impl Canvas {
+    fn new() -> Canvas {
+        Canvas { data: Data::new() }
+    }
+    fn move_to(&mut self, x: i32, y: i32) {
+        self.data.append(Command::Move(Position::Absolute, (x, y).into()));
+    }
+    
+    fn line_to(&mut self, x: i32, y: i32) {
+        self.data.append(Command::Line(Position::Relative, (x, y).into()));
+    }
+    
+    fn close(&mut self) {
+        self.data.append(Command::Close);
+    }
+}
+
 fn main() {
-    let data = Data::new()
-        .move_to((10, 10))
-        .line_by((0, 50))
-        .line_by((50, 0))
-        .line_by((0, -50))
-        .close();
+    let mut canvas = Canvas::new();
+    canvas.move_to(10, 10);
+    canvas.line_to(0, 50);
+    canvas.line_to(50, 0);
+    canvas.line_to(0, -50);
+    canvas.close();
 
     let path = Path::new()
         .set("fill", "none")
         .set("stroke", "black")
         .set("stroke-width", 3)
-        .set("d", data);
-     
+        .set("d", canvas.data);
+
     let document = Document::new()
         .set("viewBox", A4_PORTRAIT)
         .add(path);
