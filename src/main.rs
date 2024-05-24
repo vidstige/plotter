851a5e95f1d1590ed9ed81a53420c24b8ee7340f
Samuel Carlsson <samuel.carlsson@volumental.com>
2024-05-24 15:04:34 +0200

    fixup

--- src/main.rs
+++ src/main.rs
@@ -11,19 +11,6 @@ fn pad(view_box: ViewBox, pad: i32) -> ViewBox {
     (x + pad, y + pad, w - 2 * pad, h - 2 * pad)
 }
 
-struct Spiral {
-    center: Point2,
-}
-
-impl Spiral {
-    fn new(center: Point2) -> Spiral {
-        Spiral { center }
-    }
-    fn at(&self, p: Point2) -> Vector2 {
-        p.minus(&self.center).cross().scale(3.14).add(p.minus(&self.center))
-    }
-}
-
 fn random_in(rng: &mut ThreadRng, view_box: &(i32, i32, i32, i32)) -> Point2 {
     Point2::new(
         rng.gen::<f32>() * (view_box.2 - view_box.0) as f32 + view_box.0 as f32,
@@ -35,14 +22,25 @@ fn contains(view_box: &ViewBox, point: &Point2) -> bool {
     let (x, y, w, h) = view_box;
     point.x > *x as f32 && point.y > *y as f32 && point.x < (x + w) as f32 && point.y < (y + h) as f32
 }
-
+struct Spiral {
+    center: Point2,
+}
+impl Spiral {
+    fn new(center: Point2) -> Spiral {
+        Spiral { center }
+    }
+    fn at(&self, p: Point2) -> Vector2 {
+        p.minus(&self.center).cross()
+    }
+}
+    
 fn main() {
     let mut paper = Paper::new();
     // compute drawing area
     let area = pad(A4_PORTRAIT, 20);
 
-    let field = Spiral::new(Point2::new(0.5 * 210.0, 0.5 * 297.0));
     let mut rng = rand::thread_rng();
+    let field = Spiral::new(Point2::new(0.5 * 210.0, 0.5 * 297.0));
     let max_step = 2.0;
     for _ in 0..256 {
         let mut polyline = Polyline::new();
