1e9e49a99fa326875298e8a5854a85700606d1ad
Samuel Carlsson <samuel.carlsson@volumental.com>
2024-05-30 22:08:21 +0200

    refactor: template polyline dimension

--- src/polyline.rs
+++ src/polyline.rs
@@ -1,17 +1,18 @@
 use std::ops::Sub;
 
-use nalgebra_glm::Vec2;
+//use nalgebra_glm::Vec2;
+use nalgebra_glm::TVec;
 
 #[derive(Clone)]
-pub struct Polyline2 {
-    pub points: Vec<Vec2>,
+pub struct Polyline<const N: usize> {
+    pub points: Vec<TVec<f32, N>>,
 }
 
-impl Polyline2 {
-    pub fn new() -> Polyline2 {
-        Polyline2 { points: Vec::new() }
+impl<const N: usize> Polyline<N> {
+    pub fn new() -> Polyline<N> {
+        Polyline { points: Vec::new() }
     }
-    pub fn add(&mut self, point: Vec2) {
+    pub fn add(&mut self, point: TVec<f32, N>) {
         self.points.push(point);
     }
     pub fn length(&self) -> f32 {
@@ -24,3 +25,5 @@ impl Polyline2 {
         length
     }
 }
+
+pub type Polyline2 = Polyline<2>;
