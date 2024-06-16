bdfe84a3bd0b4322d7f17d85cf42203a63f8c5a2
Samuel Carlsson <samuel.carlsson@volumental.com>
2024-06-16 15:21:22 +0200

    rm expr nodes

--- src/main.rs
+++ src/main.rs
@@ -23,17 +23,12 @@ enum Node {
     /// Value node with a single output.
     /// The value is editable in UI.
     Number(f32),
-
-    /// Expression node with a single output.
-    /// It has number of inputs equal to number of variables in the expression.
-    ExprNode(ExprNode),
 }
 
 impl Node {
     fn number_out(&self) -> f32 {
         match self {
             Node::Number(value) => *value,
-            Node::ExprNode(expr_node) => expr_node.eval(),
             _ => unreachable!(),
         }
     }
@@ -45,28 +40,18 @@ impl Node {
                 1 => sigma,
                 _ => unreachable!(),
             }
-            Node::ExprNode(expr_node) => &mut expr_node.values[idx - 1],
             _ => unreachable!(),
         }
     }
 
     fn label_in(&mut self, idx: usize) -> &str {
         match self {
-            Node::ExprNode(expr_node) => &expr_node.bindings[idx - 1],
             _ => unreachable!(),
         }
     }
 
     fn string_in(&mut self) -> &mut String {
         match self {
-            Node::ExprNode(expr_node) => &mut expr_node.text,
-            _ => unreachable!(),
-        }
-    }
-
-    fn expr_node(&mut self) -> &mut ExprNode {
-        match self {
-            Node::ExprNode(expr_node) => expr_node,
             _ => unreachable!(),
         }
     }
@@ -88,7 +73,6 @@ impl SnarlViewer<Node> for NodeViewer {
             Node::NormalDistribution(_, _) => "Normal Distribution".to_owned(),
             Node::Sink => "Sink".to_owned(),
             Node::Number(_) => "Number".to_owned(),
-            Node::ExprNode(_) => "Expr".to_owned(),
         }
     }
 
@@ -97,7 +81,6 @@ impl SnarlViewer<Node> for NodeViewer {
             Node::NormalDistribution(_, _) => 2,
             Node::Sink => 1,
             Node::Number(_) => 0,
-            Node::ExprNode(expr_node) => 1 + expr_node.bindings.len(),
         }
     }
 
@@ -106,7 +89,6 @@ impl SnarlViewer<Node> for NodeViewer {
             Node::NormalDistribution(_, _) => 1,
             Node::Sink => 0,
             Node::Number(_) => 1,
-            Node::ExprNode(_) => 1,
         }
     }
 
@@ -119,8 +101,10 @@ impl SnarlViewer<Node> for NodeViewer {
     ) -> PinInfo {
         match snarl[pin.id.node] {
             Node::NormalDistribution(_, _) => {
-                let node = &mut snarl[pin.id.node];
-                ui.add(egui::DragValue::new(node.number_in(pin.id.input)));
+                if pin.remotes.len() == 0 {
+                    let node = &mut snarl[pin.id.node];
+                    ui.add(egui::DragValue::new(node.number_in(pin.id.input)));
+                }
                 PinInfo::square().with_fill(NUMBER_COLOR)
             },
             Node::Sink => {
@@ -141,119 +125,12 @@ impl SnarlViewer<Node> for NodeViewer {
                             ui.label(format_float(value));
                             PinInfo::square().with_fill(NUMBER_COLOR)
                         }
-                        Node::ExprNode(ref expr) => {
-                            assert_eq!(remote.output, 0, "Expr node has only one output");
-                            ui.label(format_float(expr.eval()));
-                            PinInfo::square().with_fill(NUMBER_COLOR)
-                        }
                     },
                     _ => unreachable!("Sink input has only one wire"),
                 }
             }
             Node::Number(_) => {
                 unreachable!("Number node has no inputs")
-            }
-            Node::ExprNode(_) if pin.id.input == 0 => {
-                let changed = match &*pin.remotes {
-                    [] => {
-                        let input = snarl[pin.id.node].string_in();
-                        let r = egui::TextEdit::singleline(input)
-                            .clip_text(false)
-                            .desired_width(0.0)
-                            .margin(ui.spacing().item_spacing)
-                            .show(ui)
-                            .response;
-
-                        r.changed()
-                    }
-                    _ => unreachable!("Expr pins has only one wire"),
-                };
-
-                if changed {
-                    let expr_node = snarl[pin.id.node].expr_node();
-
-                    match syn::parse_str(&expr_node.text) {
-                        Ok(expr) => {
-                            expr_node.expr = expr;
-
-                            let values = Iterator::zip(
-                                expr_node.bindings.iter().map(String::clone),
-                                expr_node.values.iter().copied(),
-                            )
-                            .collect::<HashMap<String, f32>>();
-
-                            let mut new_bindings = Vec::new();
-                            expr_node.expr.extend_bindings(&mut new_bindings);
-
-                            let old_bindings =
-                                std::mem::replace(&mut expr_node.bindings, new_bindings.clone());
-
-                            let new_values = new_bindings
-                                .iter()
-                                .map(|name| values.get(&**name).copied().unwrap_or(0.0))
-                                .collect::<Vec<_>>();
-
-                            expr_node.values = new_values;
-
-                            let old_inputs = (0..old_bindings.len())
-                                .map(|idx| {
-                                    snarl.in_pin(InPinId {
-                                        node: pin.id.node,
-                                        input: idx,
-                                    })
-                                })
-                                .collect::<Vec<_>>();
-
-                            for (idx, name) in old_bindings.iter().enumerate() {
-                                let new_idx =
-                                    new_bindings.iter().position(|new_name| *new_name == *name);
-
-                                match new_idx {
-                                    None => {
-                                        snarl.drop_inputs(old_inputs[idx].id);
-                                    }
-                                    Some(new_idx) if new_idx != idx => {
-                                        let new_in_pin = InPinId {
-                                            node: pin.id.node,
-                                            input: new_idx,
-                                        };
-                                        for &remote in &old_inputs[idx].remotes {
-                                            snarl.disconnect(remote, old_inputs[idx].id);
-                                            snarl.connect(remote, new_in_pin);
-                                        }
-                                    }
-                                    _ => {}
-                                }
-                            }
-                        }
-                        Err(_) => {}
-                    }
-                }
-                PinInfo::triangle().with_fill(UNTYPED_COLOR)
-            }
-            Node::ExprNode(ref expr_node) => {
-                if pin.id.input <= expr_node.bindings.len() {
-                    match &*pin.remotes {
-                        [] => {
-                            let node = &mut snarl[pin.id.node];
-                            ui.label(node.label_in(pin.id.input));
-                            ui.add(egui::DragValue::new(node.number_in(pin.id.input)));
-                            PinInfo::square().with_fill(NUMBER_COLOR)
-                        }
-                        [remote] => {
-                            let new_value = snarl[remote.node].number_out();
-                            let node = &mut snarl[pin.id.node];
-                            ui.label(node.label_in(pin.id.input));
-                            ui.label(format_float(new_value));
-                            *node.number_in(pin.id.input) = new_value;
-                            PinInfo::square().with_fill(NUMBER_COLOR)
-                        }
-                        _ => unreachable!("Expr pins has only one wire"),
-                    }
-                } else {
-                    ui.label("Removed");
-                    PinInfo::circle().with_fill(Color32::BLACK)
-                }
             }
         }
     }
@@ -276,12 +153,6 @@ impl SnarlViewer<Node> for NodeViewer {
             Node::Number(ref mut value) => {
                 assert_eq!(pin.id.output, 0, "Number node has only one output");
                 ui.add(egui::DragValue::new(value));
-                PinInfo::square().with_fill(NUMBER_COLOR)
-            }
-            Node::ExprNode(ref expr_node) => {
-                let value = expr_node.eval();
-                assert_eq!(pin.id.output, 0, "Expr node has only one output");
-                ui.label(format_float(value));
                 PinInfo::square().with_fill(NUMBER_COLOR)
             }
         }
@@ -311,9 +182,7 @@ impl SnarlViewer<Node> for NodeViewer {
             Node::Number(_) => {
                 unreachable!("Number node has no inputs")
             }
-            Node::ExprNode(_) => {
-                NUMBER_COLOR
-            }
+
         }
     }
 
@@ -329,7 +198,6 @@ impl SnarlViewer<Node> for NodeViewer {
                 unreachable!("Sink node has no outputs")
             }
             Node::Number(_) => NUMBER_COLOR,
-            Node::ExprNode(_) => NUMBER_COLOR,
         }
     }
 
@@ -347,10 +215,6 @@ impl SnarlViewer<Node> for NodeViewer {
         }
         if ui.button("Number").clicked() {
             snarl.insert_node(pos, Node::Number(0.0));
-            ui.close_menu();
-        }
-        if ui.button("Expr").clicked() {
-            snarl.insert_node(pos, Node::ExprNode(ExprNode::new()));
             ui.close_menu();
         }
         if ui.button("Sink").clicked() {
@@ -397,297 +261,6 @@ impl SnarlViewer<Node> for NodeViewer {
             }
             Node::Number(_) => {
                 ui.label("Outputs integer value");
-            }
-            Node::ExprNode(_) => {
-                ui.label("Evaluates algebraic expression with input for each unique variable name");
-            }
-        }
-    }
-}
-
-struct ExprNode {
-    text: String,
-    bindings: Vec<String>,
-    values: Vec<f32>,
-    expr: Expr,
-}
-
-impl ExprNode {
-    fn new() -> Self {
-        ExprNode {
-            text: format!("0"),
-            bindings: Vec::new(),
-            values: Vec::new(),
-            expr: Expr::Val(0.0),
-        }
-    }
-
-    fn eval(&self) -> f32 {
-        self.expr.eval(&self.bindings, &self.values)
-    }
-}
-
-enum UnOp {
-    Pos,
-    Neg,
-}
-
-#[derive(Clone, Copy)]
-enum BinOp {
-    Add,
-    Sub,
-    Mul,
-    Div,
-}
-
-enum Expr {
-    Var(String),
-    Val(f32),
-    UnOp {
-        op: UnOp,
-        expr: Box<Expr>,
-    },
-    BinOp {
-        lhs: Box<Expr>,
-        op: BinOp,
-        rhs: Box<Expr>,
-    },
-}
-
-impl Expr {
-    fn eval(&self, bindings: &[String], args: &[f32]) -> f32 {
-        let binding_index =
-            |name: &str| bindings.iter().position(|binding| binding == name).unwrap();
-
-        match self {
-            Expr::Var(ref name) => args[binding_index(name)],
-            Expr::Val(value) => *value,
-            Expr::UnOp { op, ref expr } => match op {
-                UnOp::Pos => expr.eval(bindings, args),
-                UnOp::Neg => -expr.eval(bindings, args),
-            },
-            Expr::BinOp {
-                ref lhs,
-                op,
-                ref rhs,
-            } => match op {
-                BinOp::Add => lhs.eval(bindings, args) + rhs.eval(bindings, args),
-                BinOp::Sub => lhs.eval(bindings, args) - rhs.eval(bindings, args),
-                BinOp::Mul => lhs.eval(bindings, args) * rhs.eval(bindings, args),
-                BinOp::Div => lhs.eval(bindings, args) / rhs.eval(bindings, args),
-            },
-        }
-    }
-
-    fn extend_bindings(&self, bindings: &mut Vec<String>) {
-        match self {
-            Expr::Var(name) => {
-                if !bindings.contains(name) {
-                    bindings.push(name.clone());
-                }
-            }
-            Expr::Val(_) => {}
-            Expr::UnOp { expr, .. } => {
-                expr.extend_bindings(bindings);
-            }
-            Expr::BinOp { lhs, rhs, .. } => {
-                lhs.extend_bindings(bindings);
-                rhs.extend_bindings(bindings);
-            }
-        }
-    }
-}
-
-impl syn::parse::Parse for UnOp {
-    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
-        let lookahead = input.lookahead1();
-        if lookahead.peek(syn::Token![+]) {
-            input.parse::<syn::Token![+]>()?;
-            Ok(UnOp::Pos)
-        } else if lookahead.peek(syn::Token![-]) {
-            input.parse::<syn::Token![-]>()?;
-            Ok(UnOp::Neg)
-        } else {
-            Err(lookahead.error())
-        }
-    }
-}
-
-impl syn::parse::Parse for BinOp {
-    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
-        let lookahead = input.lookahead1();
-        if lookahead.peek(syn::Token![+]) {
-            input.parse::<syn::Token![+]>()?;
-            Ok(BinOp::Add)
-        } else if lookahead.peek(syn::Token![-]) {
-            input.parse::<syn::Token![-]>()?;
-            Ok(BinOp::Sub)
-        } else if lookahead.peek(syn::Token![*]) {
-            input.parse::<syn::Token![*]>()?;
-            Ok(BinOp::Mul)
-        } else if lookahead.peek(syn::Token![/]) {
-            input.parse::<syn::Token![/]>()?;
-            Ok(BinOp::Div)
-        } else {
-            Err(lookahead.error())
-        }
-    }
-}
-
-impl syn::parse::Parse for Expr {
-    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
-        let lookahead = input.lookahead1();
-
-        let lhs;
-        if lookahead.peek(syn::token::Paren) {
-            let content;
-            syn::parenthesized!(content in input);
-            let expr = content.parse::<Expr>()?;
-            if input.is_empty() {
-                return Ok(expr);
-            }
-            lhs = expr;
-        // } else if lookahead.peek(syn::LitFloat) {
-        //     let lit = input.parse::<syn::LitFloat>()?;
-        //     let value = lit.base10_parse::<f64>()?;
-        //     let expr = Expr::Val(value);
-        //     if input.is_empty() {
-        //         return Ok(expr);
-        //     }
-        //     lhs = expr;
-        } else if lookahead.peek(syn::LitInt) {
-            let lit = input.parse::<syn::LitInt>()?;
-            let value = lit.base10_parse::<f32>()?;
-            let expr = Expr::Val(value);
-            if input.is_empty() {
-                return Ok(expr);
-            }
-            lhs = expr;
-        } else if lookahead.peek(syn::Ident) {
-            let ident = input.parse::<syn::Ident>()?;
-            let expr = Expr::Var(ident.to_string());
-            if input.is_empty() {
-                return Ok(expr);
-            }
-            lhs = expr;
-        } else {
-            let unop = input.parse::<UnOp>()?;
-
-            return Self::parse_with_unop(unop, input);
-        }
-
-        let binop = input.parse::<BinOp>()?;
-
-        Self::parse_binop(Box::new(lhs), binop, input)
-    }
-}
-
-impl Expr {
-    fn parse_with_unop(op: UnOp, input: syn::parse::ParseStream) -> syn::Result<Self> {
-        let lookahead = input.lookahead1();
-
-        let lhs;
-        if lookahead.peek(syn::token::Paren) {
-            let content;
-            syn::parenthesized!(content in input);
-            let expr = Expr::UnOp {
-                op,
-                expr: Box::new(content.parse::<Expr>()?),
-            };
-            if input.is_empty() {
-                return Ok(expr);
-            }
-            lhs = expr;
-        } else if lookahead.peek(syn::LitFloat) {
-            let lit = input.parse::<syn::LitFloat>()?;
-            let value = lit.base10_parse::<f32>()?;
-            let expr = Expr::UnOp {
-                op,
-                expr: Box::new(Expr::Val(value)),
-            };
-            if input.is_empty() {
-                return Ok(expr);
-            }
-            lhs = expr;
-        } else if lookahead.peek(syn::LitInt) {
-            let lit = input.parse::<syn::LitInt>()?;
-            let value = lit.base10_parse::<f32>()?;
-            let expr = Expr::UnOp {
-                op,
-                expr: Box::new(Expr::Val(value)),
-            };
-            if input.is_empty() {
-                return Ok(expr);
-            }
-            lhs = expr;
-        } else if lookahead.peek(syn::Ident) {
-            let ident = input.parse::<syn::Ident>()?;
-            let expr = Expr::UnOp {
-                op,
-                expr: Box::new(Expr::Var(ident.to_string())),
-            };
-            if input.is_empty() {
-                return Ok(expr);
-            }
-            lhs = expr;
-        } else {
-            return Err(lookahead.error());
-        }
-
-        let op = input.parse::<BinOp>()?;
-
-        Self::parse_binop(Box::new(lhs), op, input)
-    }
-
-    fn parse_binop(lhs: Box<Expr>, op: BinOp, input: syn::parse::ParseStream) -> syn::Result<Self> {
-        let lookahead = input.lookahead1();
-
-        let rhs;
-        if lookahead.peek(syn::token::Paren) {
-            let content;
-            syn::parenthesized!(content in input);
-            rhs = Box::new(content.parse::<Expr>()?);
-            if input.is_empty() {
-                return Ok(Expr::BinOp { lhs, op, rhs });
-            }
-        } else if lookahead.peek(syn::LitFloat) {
-            let lit = input.parse::<syn::LitFloat>()?;
-            let value = lit.base10_parse::<f32>()?;
-            rhs = Box::new(Expr::Val(value));
-            if input.is_empty() {
-                return Ok(Expr::BinOp { lhs, op, rhs });
-            }
-        } else if lookahead.peek(syn::LitInt) {
-            let lit = input.parse::<syn::LitInt>()?;
-            let value = lit.base10_parse::<f32>()?;
-            rhs = Box::new(Expr::Val(value));
-            if input.is_empty() {
-                return Ok(Expr::BinOp { lhs, op, rhs });
-            }
-        } else if lookahead.peek(syn::Ident) {
-            let ident = input.parse::<syn::Ident>()?;
-            rhs = Box::new(Expr::Var(ident.to_string()));
-            if input.is_empty() {
-                return Ok(Expr::BinOp { lhs, op, rhs });
-            }
-        } else {
-            return Err(lookahead.error());
-        }
-
-        let next_op = input.parse::<BinOp>()?;
-
-        match (op, next_op) {
-            (BinOp::Add | BinOp::Sub, BinOp::Mul | BinOp::Div) => {
-                let rhs = Self::parse_binop(rhs, next_op, input)?;
-                Ok(Expr::BinOp {
-                    lhs,
-                    op,
-                    rhs: Box::new(rhs),
-                })
-            }
-            _ => {
-                let lhs = Expr::BinOp { lhs, op, rhs };
-                Self::parse_binop(Box::new(lhs), next_op, input)
             }
         }
     }
