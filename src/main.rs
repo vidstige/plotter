d0423cc68e6e13dae735b436f6081da310e7ec7f
Samuel Carlsson <samuel.carlsson@volumental.com>
2024-06-29 10:53:00 +0200

    back to command line

--- src/main.rs
+++ src/main.rs
@@ -1,336 +1,260 @@
-use eframe::{App, CreationContext};
-use egui::{Color32, Ui, ImageData, ColorImage, TextureOptions, TextureId, Rect, pos2, Sense, Vec2, Response};
-use egui_snarl::{
-    ui::{PinInfo, SnarlStyle, SnarlViewer},
-    InPin, InPinId, NodeId, OutPin, Snarl,
-};
+use std::{ops::{Sub, Add}, io::{self, Write}, fs::File, collections::VecDeque, f64::consts::TAU};
+
+use plotter::eq::{linesearch, newton_raphson};
+use plotter::geometries::{sphere::Sphere, hole::Hole};
+use plotter::geometry::Geometry;
+use nalgebra_glm::{Vec2, Vec3, look_at, project, Vec4, perspective, unproject, Mat4};
+
+use plotter::polyline::Polyline2;
+
 use rand::{distributions::Distribution, rngs::ThreadRng};
-use rand_distr::Normal;
-use tiny_skia::Pixmap;
+use plotter::resolution::Resolution;
+use rand_distr::StandardNormal;
+use tiny_skia::{Pixmap, PathBuilder, Paint, Stroke, Transform, Color};
 
-const NUMBER_COLOR: Color32 = Color32::from_rgb(0xb0, 0x00, 0x00);
-const UNTYPED_COLOR: Color32 = Color32::from_rgb(0xb0, 0xb0, 0xb0);
-
-
-#[derive(Clone)]
-enum Kind {
-    F32,
-    USize,
-    Points2,
-    Pixmap,
+fn cross2(vector: Vec2) -> Vec2 {
+    Vec2::new(-vector.y, vector.x)
 }
 
-trait Node {
-    fn title(&self) -> &str;
-    fn inputs(&self) -> &[Kind];
-    fn outputs(&self) -> &[Kind];
-    fn input_ui(&mut self, ui: &mut Ui, pin: &InPin);
-    fn show_body(&mut self, ui: &mut Ui);
+struct Spiral {
+    center: Vec2,
 }
-
-// --- Constant ----------------------
-struct ConstantNode {
-    value: f32,    
-}
-impl ConstantNode {
-    fn new(value: f32) -> ConstantNode {
-        ConstantNode { value }
+impl Spiral {
+    fn new(center: Vec2) -> Spiral {
+        Spiral { center }
     }
-}
-impl Node for ConstantNode {
-    fn title(&self) -> &str { "Number" }
-    fn inputs(&self) -> &[Kind] { &[] }
-    fn outputs(&self) -> &[Kind] { &[Kind::F32] }
-    fn input_ui(&mut self, ui: &mut Ui, pin: &InPin) { }
-    fn show_body(&mut self, ui: &mut Ui) {
-        ui.add(egui::DragValue::new(&mut self.value));
+    fn at(&self, p: &Vec2) -> Vec2 {
+        cross2(p.sub(&self.center))
     }
 }
 
-// --- NormalDistribution ----------------------
-struct NormalDistributionNode {
-    my: f32,
-    sigma: f32,
-}
-impl NormalDistributionNode {
-    fn new(my: f32, sigma: f32) -> NormalDistributionNode {
-        NormalDistributionNode { my, sigma }
-    }
-}
-impl Node for NormalDistributionNode {
-    fn title(&self) -> &str { "Normal distribution" }
-    fn inputs(&self) -> &[Kind] { &[Kind::F32, Kind::F32] }
-    fn outputs(&self) -> &[Kind] { &[Kind::F32] }
-    fn input_ui(&mut self, ui: &mut Ui, pin: &InPin) {
-        if pin.remotes.len() > 0 {
-            return;
-        }
-        match pin.id.input {
-            0 => ui.add(egui::DragValue::new(&mut self.my)),
-            1 => ui.add(egui::DragValue::new(&mut self.sigma)),
-            _ => unreachable!("NormalDistributionNode only have two inputs"),
-        };
-    }
-    fn show_body(&mut self, _ui: &mut Ui) { }
+struct Ray {
+    origin: Vec3,
+    direction: Vec3,
 }
 
-// --- Pixmap ----------------------
-struct PixmapNode {
-    pixmap: Pixmap,
-}
-impl PixmapNode {
-    fn new(width: u32, height: u32) -> PixmapNode {
-        PixmapNode { pixmap: Pixmap::new(width, height).unwrap() }
-    }
-}
-impl Node for PixmapNode {
-    fn title(&self) -> &str { "Pixmap" }
-    fn inputs(&self) -> &[Kind] { &[Kind::Pixmap] }
-    fn outputs(&self) -> &[Kind] { &[] }
-    fn input_ui(&mut self, ui: &mut Ui, pin: &InPin) { }
-    fn show_body(&mut self, ui: &mut Ui) {
-        let pixmap = &self.pixmap;
-        let image_data = ColorImage::from_rgba_premultiplied([pixmap.width() as usize, pixmap.height() as usize], pixmap.data());
-        // TODO: Only upload here and allocate texture once
-        let texture = ui.ctx().load_texture("pixmap", image_data, TextureOptions::LINEAR);
-        let texture_id = TextureId::from(&texture);
-        let uv = Rect{ min:pos2(0.0, 0.0), max:pos2(1.0, 1.0)};
-        let (rect, _) = ui.allocate_exact_size(Vec2::new(pixmap.width() as f32, pixmap.height() as f32), Sense::hover());
-        ui.painter().image(texture_id, rect, uv, Color32::WHITE);
+impl Ray {
+    fn at(&self, t: f32) -> Vec3 {
+        self.origin.add(self.direction.scale(t))
     }
 }
 
-// --- Sample2 ----------------------
-struct Sample2Node {
-    count: usize,
+fn backproject(screen: &Vec2, model: &Mat4, projection: &Mat4, viewport: Vec4) -> Ray {
+    let world = unproject(&Vec3::new(screen.x, screen.y, 1.0), &model, &projection, viewport);
+    // recover eye position
+    let model_inverse = model.try_inverse().unwrap();
+    let eye = model_inverse.column(3).xyz();
+
+    Ray{ origin: eye, direction: world.sub(eye).normalize() }
 }
-impl Sample2Node {
-    fn new(count: usize) -> Sample2Node {
-        Sample2Node { count }
+
+trait IsoSurface {
+    fn iso_level(&self, position: &Vec3) -> f32;
+}
+
+impl IsoSurface for Hole {
+    fn iso_level(&self, position: &Vec3) -> f32 {
+        self.z(&position.xy()) - position.z
     }
 }
-impl Node for Sample2Node {
-    fn title(&self) -> &str { "Sample" }
-    fn inputs(&self) -> &[Kind] { &[Kind::USize, Kind::F32, Kind::F32] }
-    fn outputs(&self) -> &[Kind] { &[Kind::Points2] }
-    fn input_ui(&mut self, ui: &mut Ui, pin: &InPin) {
-        if pin.remotes.len() > 0 {
-            return;
-        }
-        if pin.id.input == 0 {
-            ui.add(egui::DragValue::new(&mut self.count));
+
+impl IsoSurface for Sphere {
+    fn iso_level(&self, position: &Vec3) -> f32 {
+        position.norm() - 1.0
+    }
+}
+
+fn trace<S: IsoSurface>(ray: &Ray, surface: &S, lo: f32, hi: f32) -> Option<Vec3> {
+    // first linesearch to find rough estimate
+    let f = |t| surface.iso_level(&ray.at(t));
+    if let Some((lo, hi)) = linesearch(f, lo, hi, 10) {
+        // fine tune with newton_raphson
+        if let Some(t) = newton_raphson(f, 0.5 * (hi + lo)) {
+            return Some(ray.at(t));
         }
     }
-    fn show_body(&mut self, ui: &mut Ui) { }
+    None
 }
 
-struct NodeViewer;
-
-impl SnarlViewer<Box<dyn Node>> for NodeViewer {
-    fn connect(&mut self, from: &OutPin, to: &InPin, snarl: &mut Snarl<Box<dyn Node>>) {
-        //
-        
-        for &remote in &to.remotes {
-            snarl.disconnect(remote, to.id);
-        }
-
-        snarl.connect(from.id, to.id);
-    }
-
-    fn title(&mut self, node: &Box<dyn Node>) -> String {
-        node.title().to_owned()
-    }
-
-    fn inputs(&mut self, node: &Box<dyn Node>) -> usize {
-        node.inputs().len()
-    }
-
-    fn outputs(&mut self, node: &Box<dyn Node>) -> usize {
-        node.outputs().len()
-    }
-
-    fn show_input(
-        &mut self,
-        pin: &InPin,
-        ui: &mut Ui,
-        _scale: f32,
-        snarl: &mut Snarl<Box<dyn Node>>,
-    ) -> PinInfo {
-        snarl[pin.id.node].input_ui(ui, pin);
-        PinInfo::circle().with_fill(NUMBER_COLOR)
-    }
-
-    fn show_output(
-        &mut self,
-        pin: &OutPin,
-        ui: &mut Ui,
-        _scale: f32,
-        snarl: &mut Snarl<Box<dyn Node>>,
-    ) -> PinInfo {
-        PinInfo::square().with_fill(NUMBER_COLOR)
-    }
-
-    fn input_color(
-        &mut self,
-        pin: &InPin,
-        _style: &egui::Style,
-        snarl: &mut Snarl<Box<dyn Node>>,
-    ) -> Color32 {
-        NUMBER_COLOR
-    }
-
-    fn output_color(
-        &mut self,
-        _pin: &OutPin,
-        _style: &egui::Style,
-        _snarl: &mut Snarl<Box<dyn Node>>,
-    ) -> Color32 {
-        NUMBER_COLOR
-    }
-
-    fn has_body(&mut self, node: &Box<dyn Node>) -> bool {
-        // TODO: Only if has body?
-        true
-    }
-
-    fn show_body(
-        &mut self,
-        node_id: NodeId,
-        inputs: &[InPin],
-        _outputs: &[OutPin],
-        ui: &mut Ui,
-        _scale: f32,
-        snarl: &mut Snarl<Box<dyn Node>>,
-    ) {
-        for input in inputs {
-            //let kind = &snarl[input.id.node].inputs()[input.id.input];
-            //snarl[node_id].input_value(input.id.input)
-            //node.receive();
-        }
-        snarl[node_id].show_body(ui);
-    }
-
-    fn graph_menu(
-        &mut self,
-        pos: egui::Pos2,
-        ui: &mut Ui,
-        _scale: f32,
-        snarl: &mut Snarl<Box<dyn Node>>,
-    ) {
-        ui.label("Add node");
-        if ui.button("Normal distribution").clicked() {
-            snarl.insert_node(pos, Box::new(NormalDistributionNode::new(0.0, 1.0)));
-            ui.close_menu();
-        }
-        if ui.button("Number").clicked() {
-            snarl.insert_node(pos, Box::new(ConstantNode::new(0.0)));
-            ui.close_menu();
-        }
-        if ui.button("Sample").clicked() {
-            snarl.insert_node(pos, Box::new(Sample2Node::new(256)));
-            ui.close_menu();
-        }
-        if ui.button("Pixmap").clicked() {
-            snarl.insert_node(pos, Box::new(PixmapNode::new(320, 200)));
-            ui.close_menu();
-        }
-    }
-
-    fn node_menu(
-        &mut self,
-        node: NodeId,
-        _inputs: &[InPin],
-        _outputs: &[OutPin],
-        ui: &mut Ui,
-        _scale: f32,
-        snarl: &mut Snarl<Box<dyn Node>>,
-    ) {
-        ui.label("Node menu");
-        if ui.button("Remove").clicked() {
-            snarl.remove_node(node);
-            ui.close_menu();
-        }
-    }
+fn contains(resolution: &Resolution, point: &Vec2) -> bool {
+    point.x >= 0.0 && point.x < resolution.width as f32 && point.y >= 0.0 && point.y < resolution.height as f32
 }
 
-pub struct PlotterApp {
-    snarl: Snarl<Box<dyn Node>>,
-    style: SnarlStyle,
-}
-
-impl PlotterApp {
-    pub fn new(_ctx: &CreationContext) -> Self {
-        let snarl = Snarl::new();
-        let style = SnarlStyle::new();
-        PlotterApp { snarl, style }
-    }
-}
-
-impl App for PlotterApp {
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        //egui_extras::install_image_loaders(ctx);
-
-        egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
-            // The top panel is often a good place for a menu bar:
-
-            egui::menu::bar(ui, |ui| {
-                #[cfg(not(target_arch = "wasm32"))]
-                {
-                    ui.menu_button("File", |ui| {
-                        if ui.button("Quit").clicked() {
-                            ctx.send_viewport_cmd(egui::ViewportCommand::Close)
-                        }
-                    });
-                    ui.add_space(16.0);
-                }
-
-                egui::widgets::global_dark_light_mode_switch(ui);
-            });
-        });
-
-        /*egui::SidePanel::left("style").show(ctx, |ui| {
-            egui::ScrollArea::vertical().show(ui, |ui| {
-                egui_probe::Probe::new("Snarl style", &mut self.style).show(ui);
-            });
-        });*/
-
-        egui::CentralPanel::default().show(ctx, |ui| {
-            self.snarl
-                .show(&mut NodeViewer, &self.style, egui::Id::new("snarl"), ui);
-        });
-    }
-}
-
-// When compiling natively:
-#[cfg(not(target_arch = "wasm32"))]
-fn main() -> eframe::Result<()> {
-    let native_options = eframe::NativeOptions {
-        viewport: egui::ViewportBuilder::default()
-            .with_inner_size([800.0, 600.0])
-            .with_min_inner_size([320.0, 200.0]),
-        ..Default::default()
-    };
-
-    eframe::run_native(
-        "egui-snarl demo",
-        native_options,
-        Box::new(|cx| Box::new(PlotterApp::new(cx))),
+fn sample_vec2<D: Distribution<f32>>(distribution: &D, rng: &mut ThreadRng) -> Vec2 {
+    Vec2::new(
+        distribution.sample(rng),
+        distribution.sample(rng),
     )
 }
 
-// When compiling to web using trunk:
-#[cfg(target_arch = "wasm32")]
-fn main() {
-    let web_options = eframe::WebOptions::default();
+// return Christoffel symbols with index k, i, j
+fn compute_gamma(geometry: &impl Geometry, p: &Vec2) -> [[[f32; 2]; 2]; 2] {
+    let metric = geometry.metric(p);
+    let maybe_inverse_metric = metric.try_inverse();
+    if maybe_inverse_metric.is_none() {
+        println!("could not invert {:?}", metric);
+    }
+    let inverse_metric = maybe_inverse_metric.unwrap();
+    // compute all second order partial derivatives
+    let d2: [[Vec3; 2]; 2] = [ 
+        [geometry.du().du().evaluate(p), geometry.du().dv().evaluate(p)],
+        [geometry.dv().du().evaluate(p), geometry.dv().dv().evaluate(p)],
+    ];
+    // compute first order partial derivatives
+    let d: [Vec3; 2] = [geometry.du().evaluate(p), geometry.dv().evaluate(p)];
 
-    wasm_bindgen_futures::spawn_local(async {
-        eframe::WebRunner::new()
-            .start(
-                "egui_snarl_demo",
-                web_options,
-                Box::new(|cx| Box::new(PlotterApp::new(cx))),
-            )
-            .await
-            .expect("failed to start eframe");
-    });
+    // compute tensor product gamma^k_ij = (d²R/du^i du^j) * (dR/du^l) * (g^-1)^lk
+    // the index l is thus summed over
+    let mut tmp = [[[0.0; 2]; 2]; 2];
+    for k in 0..2 {
+        for i in 0..2 {
+            for j in 0..2 {
+                for l in 0..2 {
+                    tmp[k][i][j] += d2[i][j].dot(&d[l]) * inverse_metric[(l, k)];
+                }
+            }
+        }
+    }
+    tmp
+}
+
+fn acceleration(geometry: &impl Geometry, position: &Vec2, velocity: &Vec2) -> Vec2 {
+    let gamma = compute_gamma(geometry, position);
+    let mut a = Vec2::zeros();
+    let u = velocity.as_slice();
+    // tensor sum
+    for k in 0..2 {
+        for i in 0..2 {
+            for j in 0..2 {
+                a.as_mut_slice()[k] += -gamma[k][i][j] * u[i] * u[j];
+            }
+        }
+    }
+    a
+}
+
+struct Particle {
+    position: Vec2,
+    velocity: Vec2,
+}
+
+fn main() -> io::Result<()> {
+    let resolution = Resolution::new(506, 253);
+
+    let mut rng = rand::thread_rng();
+    let distribution = StandardNormal {};
+    let field = Spiral::new(Vec2::zeros());
+
+    let eye = Vec3::new(-2.5, -2.5, -1.5);
+    let model = look_at(&eye, &Vec3::new(0.0, 0.0, 0.8), &Vec3::new(0.0, 0.0, 1.0));
+    let near = 0.1;
+    let far = 10.0;
+    let projection = perspective(resolution.aspect_ratio(), 45.0_f32.to_radians(), near, far);
+    let viewport = Vec4::new(0.0, 0.0, resolution.width as f32, resolution.height as f32);
+
+    let geometry = Hole::new();
+    //let geometry = Sphere::new();
+
+    let mut output = File::create(std::path::Path::new("output.raw"))?;
+    let positions: Vec<_> = (0..256).map(|_| sample_vec2(&distribution, &mut rng)).collect();
+    //let semicircle = Uniform::new(0.0, 0.5*TAU).unwrap();
+    //let positions: Vec<_> = (0..256).map(|_| sample_vec2(&semicircle, &mut rng)).collect();
+    let mut particles: Vec<_> = positions.iter().map(|p| Particle {
+        position: Vec2::new(p.x, p.y),
+        //velocity: 0.2 * field.at(p),
+        velocity: Vec2::new(0.0, 0.5),
+    }).collect();
+    let mut traces: Vec<VecDeque<Vec3>> = particles.iter().map(|_| VecDeque::new()).collect();
+    let fps = 25.0;
+    let dt = 1.0 / fps;
+    for frame in 0..256 {
+        let mut polylines = Vec::new();
+        for (index, particle) in particles.iter_mut().enumerate() {
+            // move according to field
+            /*let nabla = field.at(p);
+            let norm = nabla.norm();
+            let step = 0.01;
+            p.add_assign(nabla.scale(step / norm));*/
+
+            // integrate geodesic equation (d²u/dt²) + gamma^k_ij * (du^i/dt) * (du^j/dt) = 0
+            
+            // verlet
+            /*let a = acceleration(&geometry, &particle.position, &particle.velocity);
+            let new_position = particle.position + particle.velocity * dt + a * (dt * dt * 0.5);
+            let new_a = acceleration(&geometry, &new_position, &particle.velocity);
+            let new_velocity = particle.velocity + (a + new_a) * (dt * 0.5);
+            particle.position = new_position;
+            particle.velocity = new_velocity;
+            // TODO: acceleration could be stored to save time
+            */
+            
+            // euler
+            let a = acceleration(&geometry, &particle.position, &particle.velocity);
+            particle.position += particle.velocity * dt;
+            particle.velocity += a * dt;
+
+            // evaluate surface at x, y
+            //let z = hole.z(&p);
+            //let world = Vec3::new(p.x, p.y, z);
+            
+            // project world cordinate into screen cordinate
+            let world = geometry.evaluate(&particle.position);
+            let screen = project(&world, &model, &projection, viewport);
+
+            traces[index].push_back(screen);
+            if traces[index].len() > 10 {
+                traces[index].pop_front();
+            }
+        }
+
+        // draw traces
+        if frame > 10 {
+            for particle_trace in &traces {
+                let mut polyline = Polyline2::new();
+                for screen in particle_trace {
+                    if contains(&resolution, &screen.xy()) {
+                        // back project and ray trace to find occlusions
+                        let ray = backproject(&screen.xy(), &model, &projection, viewport);
+                        if let Some(intersection) = trace(&ray, &geometry, near, far) {
+                            let traced_screen = project(&intersection, &model, &projection, viewport);
+                            // handle occlusions
+                            if screen.z - traced_screen.z < 0.0001 {
+                                polyline.add(screen.xy());
+                            }
+                        }
+                    }
+                }
+                polylines.push(polyline);
+            }
+        }
+
+        // render to pixmap
+        let mut pixmap = Pixmap::new(resolution.width, resolution.height).unwrap();
+        
+        let color = Color::from_rgba8(255, 180, 220, 0xff);
+        let mut paint = Paint::default();
+        paint.set_color(color);
+        paint.anti_alias = true;
+
+        let mut stroke = Stroke::default();
+        stroke.width = 1.0;
+
+        for polyline in polylines {
+            let mut pb = PathBuilder::new();
+            for (index, point) in polyline.points.iter().enumerate() {
+                if index == 0 {
+                    pb.move_to(point.x, point.y);
+                } else {
+                    pb.line_to(point.x, point.y);
+                }
+            }
+            if let Some(path) = pb.finish() {
+                pixmap.stroke_path(&path, &paint, &stroke, Transform::identity(), None);
+            }
+        }
+        //pixmap.save_png("image.png")?;
+        output.write_all(pixmap.data())?;
+    }
+
+    Ok(())
 }
