f63da3792de2551cab9cc598c2d1e93e39faf8aa
Samuel Carlsson <samuel.carlsson@gmail.com>
2025-04-28 20:09:02 +0200

    visualize local affine connection with grid lines

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,10 +1,9 @@
-use std::{f32::consts::TAU, io};
+use std::{io, ops::AddAssign};
 
-use nalgebra_glm::{cross2d, look_at, perspective, project, Mat4, Mat4x4, Vec2, Vec3, Vec4};
-use plotter::{fields::Spiral, geometries::hole::Hole, geometry::{self, Geometry}, integrate::verlet, iso_surface::IsoSurface, paper::{pad, viewbox_aspect, Paper, ViewBox, A4_LANDSCAPE}, polyline::Polyline2, raytracer::{backproject, trace}};
+use nalgebra_glm::{look_at, perspective, project, Mat4x4, Vec2, Vec3, Vec4};
+use plotter::{fields::Spiral, geometries::hole::Hole, geometry::{compute_gamma, Geometry}, integrate::verlet, iso_surface::IsoSurface, paper::{pad, viewbox_aspect, Paper, ViewBox, A4_LANDSCAPE}, polyline::Polyline2, raytracer::{backproject, trace}};
 use rand::rngs::ThreadRng;
 use rand_distr::{Distribution, Normal};
-use svg::node::element::path::Position;
 
 fn contains(view_box: &ViewBox, point: &Vec2) -> bool {
     let (x, y, w, h) = view_box;
@@ -47,6 +46,47 @@ struct Particle {
     velocity: Vec2,
 }
 
+fn grid_line_offset_at(gamma: &[[[f32; 2]; 2]; 2], direction: Vec2) -> Vec2 {
+    let d = direction;
+    let mut offset = Vec2::zeros();
+    for i in 0..2 {
+        offset[i] = gamma[i][0][0] * d.x * d.x
+                  + 2.0 * gamma[i][0][1] * d.x * d.y
+                  + gamma[i][1][1] * d.y * d.y;
+    }
+    offset
+}
+
+fn grid_line(
+    geometry: &impl Geometry,
+    start: Vec2,
+    end: Vec2,
+    n: usize,
+    amplitude: f32,
+) -> Polyline2 {
+    let mut polyline = Polyline2::new();
+
+    let direction = (end - start).normalize();
+    for i in 0..n {
+        let t = i as f32 / (n - 1) as f32;
+        // lerp position between start & end
+        let p = (1.0 - t) * start + t * end;
+
+        // Compute Christoffel symbols at current point
+        let gamma = compute_gamma(geometry, &p);
+        // Determine how much sideways push we want
+        let offset = grid_line_offset_at(&gamma, direction);
+
+        // Apply the sideways offset and add to polyline
+        let pos = p + direction * amplitude + 0.5 * offset * amplitude * amplitude;
+
+        polyline.add(pos);
+        //polyline.add(p);
+    }
+
+    polyline
+}
+
 fn main() -> io::Result<()> {
     // set up paper
     let mut paper = Paper::new(A4_LANDSCAPE, 0.5);
@@ -63,16 +103,28 @@ fn main() -> io::Result<()> {
     let geometry = Hole::new();
 
     // set up 3D camera
-    let eye = Vec3::new(-2.5, -2.5, -1.5);
-    let model = look_at(&eye, &Vec3::new(0.0, 0.0, 0.8), &Vec3::new(0.0, 0.0, 1.0));
+    //let eye = Vec3::new(-2.5, -2.5, -1.5);
+    //let model = look_at(&eye, &Vec3::new(0.0, 0.0, 0.8), &Vec3::new(0.0, 0.0, 1.0));
+    let eye = Vec3::new(0.0, 0.0, -2.5);
+    let model = look_at(&eye, &Vec3::new(0.0, 0.0, 0.0), &Vec3::new(0.0, 1.0, 0.0));
+
     let near = 0.1;
     let far = 10.0;
     let projection = perspective(viewbox_aspect(paper.view_box), 45.0_f32.to_radians(), near, far);
     let viewport = Vec4::new(area.0 as f32, area.1 as f32, area.2 as f32, area.3 as f32);
     let camera = Camera { projection, model, viewport };
 
-    let n = 64;
+    let size = 1.5;  // parameter for square things like gridlines
+    let n = 16;
     for i in 0..n {
+        // gridlines
+        let amplitude = 0.05;
+
+        let p = size * 2.0 * (i as f32 / n as f32 - 0.5);
+        let start = Vec2::new(-size, p);
+        let end = Vec2::new(size, p);
+        let uv_polyline = grid_line(&geometry, start, end, 32, amplitude);
+
         // random positions
         /*let position = Vec2::new(
             distribution.sample(&mut rng) as f32,
@@ -85,11 +137,10 @@ fn main() -> io::Result<()> {
         };*/
 
         // paralell lines
-        let size = 1.5;
-        let mut particle = Particle {
+        /*let mut particle = Particle {
             position: Vec2::new(-size, size * 2.0 * (i as f32 / n as f32 - 0.5)),
             velocity: Vec2::new(1.0, 0.0),
-        };
+        };*/
 
         // inward
         /*let theta = TAU * (i as f32 / n as f32);
@@ -98,12 +149,12 @@ fn main() -> io::Result<()> {
         let mut particle = Particle {position, velocity};*/
 
         // integrate
-        let mut uv_polyline = Polyline2::new();
+        /*let mut uv_polyline = Polyline2::new();
         for _ in 0..20 {
             uv_polyline.add(particle.position);
             let dt = 0.1;
             (particle.position, particle.velocity) = verlet(&geometry, &particle.position, &particle.velocity, dt);
-        }
+        }*/
 
         // project & etc
         let points: Vec<_> = uv_polyline.points.iter()
@@ -117,7 +168,6 @@ fn main() -> io::Result<()> {
         let polyline = Polyline2 { points };
         paper.add(polyline);
     }
-
     paper.optimize();
     paper.save("output.svg")?;
 
