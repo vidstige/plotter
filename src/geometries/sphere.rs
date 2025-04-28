b29f9af51813e9112f89bc50b357e526c1a49277
Samuel Carlsson <samuel.carlsson@gmail.com>
2025-04-28 09:35:22 +0200

    refactor: reorder stuff

--- src/geometries/sphere.rs
+++ src/geometries/sphere.rs
@@ -2,103 +2,10 @@ use nalgebra_glm::{Vec2, Vec3, Mat2x2};
 
 use crate::{geometry::Geometry, iso_surface::IsoSurface};
 
-
-pub struct Sphere {
-
-}
+pub struct Sphere;
 impl Sphere {
     pub fn new() -> Sphere {
         Sphere { }
-    }
-}
-
-// second derivatives of sphere
-struct SphereDuDu {
-
-}
-impl Geometry for SphereDuDu {
-    fn evaluate(&self, p: &Vec2) -> Vec3 {
-        let (u, v) = (p.x, p.y);
-        return Vec3::new(
-           -v.cos() * u.sin(),
-           -v.sin() * u.sin(),
-           -u.cos(),
-        )
-    }
-}
-
-struct SphereDvDv {
-
-}
-impl Geometry for SphereDvDv {
-    fn evaluate(&self, p: &Vec2) -> Vec3 {
-        let (u, v) = (p.x, p.y);
-        return Vec3::new(
-          -v.cos() * u.sin(),
-          -v.sin() * u.sin(),
-          0.0,
-        )
-    }
-}
-
-struct SphereDuDv {
-
-}
-
-impl Geometry for SphereDuDv {
-    fn evaluate(&self, p: &Vec2) -> Vec3 {
-        let (u, v) = (p.x, p.y);
-        return Vec3::new(
-            -v.sin() * u.cos(),
-            v.cos() * u.cos(),
-            0.0,
-        )
-    }
-}
-
-// first derivatives of sphere
-struct SphereDu {
-
-}
-impl Geometry for SphereDu {
-    fn evaluate(&self, p: &Vec2) -> Vec3 {
-        let (u, v) = (p.x, p.y);
-        return Vec3::new(
-            v.cos() * u.cos(),
-            v.sin() * u.cos(),
-            -u.sin(),
-        )
-    }
-
-    fn du(&self) -> impl Geometry {
-        SphereDuDu {}
-    }
-
-    fn dv(&self) -> impl Geometry {
-        SphereDuDv {}
-    }
-}
-
-struct SphereDv {
-
-}
-impl Geometry for SphereDv {
-    fn evaluate(&self, p: &Vec2) -> Vec3 {
-        let (u, v) = (p.x, p.y);
-        return Vec3::new(
-            -v.sin() * u.sin(),
-            v.cos() * u.sin(),
-            0.0,
-        )
-    }
-
-    fn du(&self) -> impl Geometry {
-        // Order of derivation does not matter, so just reuse (d/du)(d/dv)
-        SphereDuDv {}
-    }
-
-    fn dv(&self) -> impl Geometry {
-        SphereDvDv {}
     }
 }
 
@@ -129,5 +36,85 @@ impl Geometry for Sphere {
 impl IsoSurface for Sphere {
     fn iso_level(&self, position: &Vec3) -> f32 {
         position.norm() - 1.0
+    }
+}
+
+// first derivatives of sphere
+struct SphereDu;
+impl Geometry for SphereDu {
+    fn evaluate(&self, p: &Vec2) -> Vec3 {
+        let (u, v) = (p.x, p.y);
+        return Vec3::new(
+            v.cos() * u.cos(),
+            v.sin() * u.cos(),
+            -u.sin(),
+        )
+    }
+
+    fn du(&self) -> impl Geometry {
+        SphereDuDu {}
+    }
+
+    fn dv(&self) -> impl Geometry {
+        SphereDuDv {}
+    }
+}
+
+struct SphereDv;
+impl Geometry for SphereDv {
+    fn evaluate(&self, p: &Vec2) -> Vec3 {
+        let (u, v) = (p.x, p.y);
+        return Vec3::new(
+            -v.sin() * u.sin(),
+            v.cos() * u.sin(),
+            0.0,
+        )
+    }
+
+    fn du(&self) -> impl Geometry {
+        // Order of derivation does not matter, so just reuse (d/du)(d/dv)
+        SphereDuDv {}
+    }
+
+    fn dv(&self) -> impl Geometry {
+        SphereDvDv {}
+    }
+}
+
+// second derivatives of sphere
+struct SphereDuDu;
+impl Geometry for SphereDuDu {
+    fn evaluate(&self, p: &Vec2) -> Vec3 {
+        let (u, v) = (p.x, p.y);
+        return Vec3::new(
+           -v.cos() * u.sin(),
+           -v.sin() * u.sin(),
+           -u.cos(),
+        )
+    }
+}
+
+struct SphereDvDv;
+impl Geometry for SphereDvDv {
+    fn evaluate(&self, p: &Vec2) -> Vec3 {
+        let (u, v) = (p.x, p.y);
+        return Vec3::new(
+          -v.cos() * u.sin(),
+          -v.sin() * u.sin(),
+          0.0,
+        )
+    }
+}
+
+struct SphereDuDv;
+
+impl Geometry for SphereDuDv {
+    fn evaluate(&self, p: &Vec2) -> Vec3 {
+        let (u, v) = (p.x, p.y);
+        return Vec3::new(
+            -v.sin() * u.cos(),
+            v.cos() * u.cos(),
+            0.0,
+        )
     }
 }
