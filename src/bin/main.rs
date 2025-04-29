f0282bfc65f636e355643a61bb60e7b1846edf24
Samuel Carlsson <samuel.carlsson@gmail.com>
2025-04-29 21:45:29 +0200

    refactor: allow splitting polylines when reprojecting

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,7 +1,7 @@
 use std::{io, ops::AddAssign};
 
 use nalgebra_glm::{look_at, perspective, project, Mat4x4, Vec2, Vec3, Vec4};
-use plotter::{fields::Spiral, geometries::hole::Hole, geometry::Geometry, gridlines::bent_grid_line, integrate::verlet, iso_surface::IsoSurface, paper::{pad, viewbox_aspect, Paper, ViewBox, A4_LANDSCAPE}, polyline::Polyline2, raytracer::{backproject, trace}};
+use plotter::{fields::Spiral, geometries::hole::Hole, geometry::Geometry, gridlines::{bent_grid_line, grid_line}, integrate::verlet, iso_surface::IsoSurface, paper::{pad, viewbox_aspect, Paper, ViewBox, A4_LANDSCAPE}, polyline::Polyline2, raytracer::{backproject, trace}};
 use rand::rngs::ThreadRng;
 use rand_distr::{Distribution, Normal};
 
@@ -51,16 +51,17 @@ struct Particle {
 // 2. Project using camera
 // 3. Clip to viewport 
 // 4. Handle occlusion
-fn reproject<G: Geometry + IsoSurface>(polyline: &Polyline2, geometry: &G, camera: &Camera, area: ViewBox, near: f32, far: f32) -> Polyline2 {
+fn reproject<G: Geometry + IsoSurface>(polyline: &Polyline2, geometry: &G, camera: &Camera, area: ViewBox, near: f32, far: f32) -> Vec<Polyline2> {
+    // TODO: When a point is occluded, start a new linesegment
     let points: Vec<_> = polyline.points.iter()
-    .map(|uv| geometry.evaluate(uv))  // evaluate to 3D point
-    .map(|world| camera.project(world))
-    .filter(|&screen| contains(&area, &screen.xy()))
-    .filter(|&screen| visible(&screen, &camera, geometry, near, far)) // handle occlusions
-    .map(|screen| screen.xy())
-    .collect();
+        .map(|uv| geometry.evaluate(uv))  // evaluate to 3D point
+        .map(|world| camera.project(world))
+        .filter(|&screen| contains(&area, &screen.xy()))
+        .filter(|&screen| visible(&screen, &camera, geometry, near, far)) // handle occlusions
+        .map(|screen| screen.xy())
+        .collect();
 
-    Polyline2 { points }
+    [Polyline2 { points }].into()
 }
 
 fn main() -> io::Result<()> {
@@ -79,10 +80,11 @@ fn main() -> io::Result<()> {
     let geometry = Hole::new();
 
     // set up 3D camera
-    //let eye = Vec3::new(-2.5, -2.5, -1.5);
-    //let model = look_at(&eye, &Vec3::new(0.0, 0.0, 0.8), &Vec3::new(0.0, 0.0, 1.0));
-    let eye = Vec3::new(0.0, 0.0, -2.5);
-    let model = look_at(&eye, &Vec3::new(0.0, 0.0, 0.0), &Vec3::new(0.0, 1.0, 0.0));
+    let eye = Vec3::new(-2.5, -2.5, -1.5);
+    let model = look_at(&eye, &Vec3::new(0.0, 0.0, 0.8), &Vec3::new(0.0, 0.0, 1.0));
+    // top-view
+    //let eye = Vec3::new(0.0, 0.0, -2.5);
+    //let model = look_at(&eye, &Vec3::new(0.0, 0.0, 0.0), &Vec3::new(0.0, 1.0, 0.0));
 
     let near = 0.1;
     let far = 10.0;
@@ -99,13 +101,20 @@ fn main() -> io::Result<()> {
         // fixed v gridlines
         let start = Vec2::new(-size, p);
         let end = Vec2::new(size, p);
-        let uv_polyline = bent_grid_line(&geometry, start, end, amplitude);
-        paper.add(reproject(&uv_polyline, &geometry, &camera, area, near, far));
+        //let uv_polyline = bent_grid_line(&geometry, start, end, amplitude);
+        let uv_polyline = grid_line(start, end, 128);
+        for polyline in reproject(&uv_polyline, &geometry, &camera, area, near, far) {
+            paper.add(polyline);
+        }
         // fixed u gridlines
         let start = Vec2::new(p, -size);
         let end = Vec2::new(p, size);
-        let uv_polyline = bent_grid_line(&geometry, start, end, amplitude);
-        paper.add(reproject(&uv_polyline, &geometry, &camera, area, near, far));
+        //let uv_polyline = bent_grid_line(&geometry, start, end, amplitude);
+        let uv_polyline = grid_line(start, end, 128);
+        for polyline in reproject(&uv_polyline, &geometry, &camera, area, near, far) {
+            paper.add(polyline);
+        }
+
 
         // random positions
         /*let position = Vec2::new(
