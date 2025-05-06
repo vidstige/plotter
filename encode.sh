3e6dc14a805130935a14073bb2f85e760a96074d
Samuel Carlsson <samuel.carlsson@gmail.com>
2025-05-06 20:47:49 +0200

    working twitter/x encoding

--- encode.sh
+++ encode.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-RESOLUTION=${RESOLUTION:-506x253}
+RESOLUTION=${RESOLUTION:-720x720}
 FPS=${FPS:-30}
 if [[ $1 == *.gif ]]; then
     VF=-vf "fps=$FPS,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse"
 else
-    VF="-crf 15"
+    VF=-"c:v libx264 -crf 20 -preset slow -vf format=yuv420p"
 fi
-echo ffmpeg -hide_banner -f rawvideo -pixel_format rgb32 -framerate $FPS -video_size $RESOLUTION -i - -metadata comment="vidstige" $VF $1
-ffmpeg -hide_banner -f rawvideo -pixel_format rgb32 -framerate $FPS -video_size $RESOLUTION -i - -metadata comment="vidstige" $VF $1
+
+ffmpeg -hide_banner -f rawvideo -pixel_format rgb32 -r $FPS  -framerate $FPS -video_size $RESOLUTION -i - $VF $1
