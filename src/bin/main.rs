177de77086431113020c18dc935805686df691a4
Samuel Carlsson <samuel.carlsson@gmail.com>
2025-05-28 21:17:27 +0200

    compute uv polylines separately

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -27,7 +27,7 @@ fn simulate(
     uv_polylines
 }
 
-fn setup_torus(view_box: ViewBox, area: ViewBox, rng: &mut impl Rng) -> (Torus, Camera, Vec<Polyline2>) {
+fn setup_torus(view_box: ViewBox, area: ViewBox, rng: &mut impl Rng) -> (Torus, Camera) {
     let geometry = Torus::new(0.5, 1.0);
 
     let eye = Vec3::new(-2.2, -2.2, -1.2);
@@ -39,9 +39,7 @@ fn setup_torus(view_box: ViewBox, area: ViewBox, rng: &mut impl Rng) -> (Torus, 
     let viewport = Vec4::new(area.0 as f32, area.1 as f32, area.2 as f32, area.3 as f32);
     let camera = Camera { projection, model, viewport };
 
-    let uv_polylines = generate_grid((0.0, TAU), (0.0, TAU), 48, 128);
-
-    (geometry, camera, uv_polylines)
+    (geometry, camera)
 }
 
 fn sample_vec2<D: Distribution<f32>>(distribution: &D, rng: &mut impl Rng) -> Vec2 {
@@ -51,7 +49,7 @@ fn sample_vec2<D: Distribution<f32>>(distribution: &D, rng: &mut impl Rng) -> Ve
     )
 }
 
-fn setup_gaussian(view_box: ViewBox, area: ViewBox, rng: &mut impl Rng) -> (Gaussian, Camera, Vec<Polyline2>) {
+fn setup_gaussian(view_box: ViewBox, area: ViewBox, rng: &mut impl Rng) -> (Gaussian, Camera) {
     let geometry = Gaussian;
 
     let eye = Vec3::new(-1.8, -1.8, -1.2);
@@ -62,26 +60,7 @@ fn setup_gaussian(view_box: ViewBox, area: ViewBox, rng: &mut impl Rng) -> (Gaus
     let projection = perspective(viewbox_aspect(view_box), 45.0_f32.to_radians(), near, far);
     let viewport = Vec4::new(area.0 as f32, area.1 as f32, area.2 as f32, area.3 as f32);
     let camera = Camera { projection, model, viewport };
-
-    // set up initial positions and velocities
-    let distribution = Normal::new(0.0, 1.0).unwrap();
-    let n = 100;
-    let positions: Vec<_> = (0..n)
-        .map(|i| TAU * i as f32 / n as f32)
-        .map(|a| {
-            let r = 2.0;
-            Vec2::new(r * a.cos(), r * a.sin())
-        })
-        .collect();
-    let velocities: Vec<_> = positions
-        .iter()
-        .map(|p| 0.5 * cross2(*p).normalize() - p)
-        .map(|p| p + 0.125 * sample_vec2(&distribution, rng))
-        .collect();
-
-    let uv_polylines = simulate(&geometry, positions, velocities, 0.05, 32);
-
-    (geometry, camera, uv_polylines)
+    (geometry, camera)
 }
 
 fn setup_hole(view_box: ViewBox, area: ViewBox) -> (Hole, Camera, Vec<Polyline2>) {
@@ -100,6 +79,26 @@ fn setup_hole(view_box: ViewBox, area: ViewBox) -> (Hole, Camera, Vec<Polyline2>
     let uv_polylines = generate_grid((-size, size), (-size, size), 32, 256);
 
     (geometry, camera, uv_polylines)
+}
+
+fn simulate_abc(geometry: &impl DifferentiableGeometry, rng: &mut impl Rng) -> Vec<Polyline2> {
+    // set up initial positions and velocities
+    let distribution = Normal::new(0.0, 1.0).unwrap();
+    let n = 100;
+    let positions: Vec<_> = (0..n)
+        .map(|i| TAU * i as f32 / n as f32)
+        .map(|a| {
+            let r = 2.0;
+            Vec2::new(r * a.cos(), r * a.sin())
+        })
+        .collect();
+    let velocities: Vec<_> = positions
+        .iter()
+        .map(|p| 0.5 * cross2(*p).normalize() - p)
+        .map(|p| p + 0.125 * sample_vec2(&distribution, rng))
+        .collect();
+
+    simulate(geometry, positions, velocities, 0.05, 32)
 }
 
 fn format_duration(duration: Duration) -> String {
@@ -125,9 +124,10 @@ fn main() -> io::Result<()> {
     let near = 0.1;
     let far = 10.0;
 
-    //let (geometry, camera, uv_polylines) = setup_torus(paper.view_box, area, &mut rng);
-    //let (geometry, camera, uv_polylines) = setup_gaussian(paper.view_box, area, &mut rng);
-    let (geometry, camera, uv_polylines) = setup_hole(paper.view_box, area);
+    //let (geometry, camera) = setup_torus(paper.view_box, area, &mut rng);
+    let (geometry, camera) = setup_gaussian(paper.view_box, area, &mut rng);
+    //let (geometry, camera) = setup_hole(paper.view_box, area);
+    let uv_polylines = generate_grid((-2.0, 2.0), (-2.0, 2.0), 64, 256);
 
     for uv_polyline in uv_polylines {
         for polyline in reproject(&uv_polyline, &geometry, &camera, area, near, far) {
