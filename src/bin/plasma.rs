ab0d919b258b2d6b664ccb2a8934ec14bfa925d7
Samuel Carlsson <samuel.carlsson@gmail.com>
2025-09-21 21:18:49 +0200

    lerp in pattern

--- src/bin/plasma.rs
+++ src/bin/plasma.rs
@@ -40,12 +40,25 @@ fn linspace(lo: f32, hi: f32, n: usize) -> Vec<f32> {
     (0..n).map(|i| lo + size * (i as f32) / n as f32).collect()
 }
 
+fn hann(t: f32) -> f32 {
+    (0.5 - 0.5 * (std::f32::consts::TAU * t).cos()).max(0.0)
+}
+
 fn main() -> io::Result<()> {
     let mut output = io::stdout().lock();
+
+    // Load mask
+    let pattern = Pixmap::load_png("data/volumental.png")?;
+    let tmp = sdf_from_pixmap(&pattern);
+    let lo = tmp.values.iter().fold(f32::INFINITY, |a, &b| a.min(b));
+    let hi = tmp.values.iter().fold(-f32::INFINITY, |a, &b| a.max(b));
+    let scale = hi.abs().max(lo.abs());
+    let pattern_sdf = tmp * (1.0 / scale);
 
     let resolution = Resolution::new(720, 720);
     let mut pixmap = Pixmap::new(resolution.width, resolution.height).unwrap();
 
+    // setup stroke & paint
     let color = Color::BLACK;
     let mut paint = Paint::default();
     paint.set_color(color);
@@ -55,17 +68,21 @@ fn main() -> io::Result<()> {
     stroke.width = 8.0;
 
     let levels = linspace(-1.0, 1.0, 8);
-    let frames = 512;
+    let frames = 256;
     let speed = 4.0;
     for frame in 0..frames {
         let t = frame as f32 / frames as f32;
         let z = speed * t;
 
+        let pattern_resolution = Resolution::new(pattern.width(), pattern.height());
+        let simplex_sdf = sample_at(&pattern_resolution, z);
+        // combine pattern sdf with field sdf
+        let s = hann(t);
+        let field = simplex_sdf * (1.0 - s) + &(&pattern_sdf * s);
+
         pixmap.fill(Color::WHITE);
-        let field = sample_at(&resolution, z);
         for level in &levels {
             let polylines = find_contours(&field, *level);
-
             for polyline in polylines {
                 draw_polyline(&mut pixmap, polyline, &paint, &stroke);
             }
@@ -74,14 +91,6 @@ fn main() -> io::Result<()> {
         output.write_all(pixmap.data())?;
         output.flush()?;
     }
-
-    /*let mask = Pixmap::load_png("data/volumental.png")?;
-    let sdf = sdf_from_pixmap(&mask);
-    let polylines = find_contours(&sdf, 0.0);
-    for polyline in polylines {
-        draw_polyline(&mut pixmap, polyline, &paint, &stroke);
-    }
-    pixmap.save_png("output.png")?;*/
 
     Ok(())
 }
--- src/field.rs
+++ src/field.rs
@@ -1,4 +1,4 @@
-use std::ops::Index;
+use std::ops::{Add, Index, Mul};
 
 use crate::resolution::Resolution;
 
@@ -27,4 +27,84 @@ impl<T: Clone> Field<T> {
         let area = resolution.area();
         Field { resolution, values: vec![value; area] }
     }
+}
+
+impl<T> Add for Field<T>
+where
+    T: Add<Output = T>,
+{
+    type Output = Field<T>;
+
+    fn add(self, rhs: Field<T>) -> Field<T> {
+        let Field { resolution, values } = self;
+        let Field { resolution: rhs_resolution, values: rhs_values } = rhs;
+
+        assert_same_resolution(&resolution, &rhs_resolution);
+        combine_fields(resolution, values, rhs_values, |lhs, rhs| lhs + rhs)
+    }
+}
+
+impl<'a, T> Add<&'a Field<T>> for Field<T>
+where
+    T: Add<Output = T> + Clone,
+{
+    type Output = Field<T>;
+
+    fn add(self, rhs: &'a Field<T>) -> Field<T> {
+        let Field { resolution, values } = self;
+
+        assert_same_resolution(&resolution, &rhs.resolution);
+        combine_fields(resolution, values, rhs.values.iter().cloned(), |lhs, rhs| lhs + rhs)
+    }
+}
+
+impl<T, S> Mul<S> for Field<T>
+where
+    T: Mul<S, Output = T>,
+    S: Clone,
+{
+    type Output = Field<T>;
+
+    fn mul(self, rhs: S) -> Field<T> {
+        let Field { resolution, values } = self;
+        scale_field(resolution, values, rhs)
+    }
+}
+
+impl<'a, T, S> Mul<S> for &'a Field<T>
+where
+    T: Mul<S, Output = T> + Clone,
+    S: Clone,
+{
+    type Output = Field<T>;
+
+    fn mul(self, rhs: S) -> Field<T> {
+        scale_field(self.resolution.clone(), self.values.iter().cloned(), rhs)
+    }
+}
+
+fn assert_same_resolution(lhs: &Resolution, rhs: &Resolution) {
+    assert_eq!(lhs.width, rhs.width, "field width mismatch");
+    assert_eq!(lhs.height, rhs.height, "field height mismatch");
+}
+
+fn combine_fields<T, L, R, F>(resolution: Resolution, lhs: L, rhs: R, mut f: F) -> Field<T>
+where
+    L: IntoIterator<Item = T>,
+    R: IntoIterator<Item = T>,
+    F: FnMut(T, T) -> T,
+{
+    let lhs_iter = lhs.into_iter();
+    let rhs_iter = rhs.into_iter();
+    Field { resolution, values: lhs_iter.zip(rhs_iter).map(|(l, r)| f(l, r)).collect() }
+}
+
+fn scale_field<T, S, I>(resolution: Resolution, values: I, scalar: S) -> Field<T>
+where
+    I: IntoIterator<Item = T>,
+    S: Clone,
+    T: Mul<S, Output = T>,
+{
+    let iter = values.into_iter();
+    Field { resolution, values: iter.map(|value| value * scalar.clone()).collect() }
 }
