37e90cd3ad984c8c029fa6fa79a8254217c21565
Samuel Carlsson <samuel.carlsson@gmail.com>
2025-10-05 19:28:20 +0200

    support playing music with video

--- play.sh
+++ play.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 RESOLUTION=${RESOLUTION:-720x720}
-ffplay -v warning -f rawvideo -pixel_format rgb32 -framerate 30 -video_size $RESOLUTION -i -
+
+if [ $# -eq 0 ]; then
+    exec ffplay -v warning -f rawvideo -pixel_format rgb32 -framerate 30 -video_size "$RESOLUTION" -i -
+fi
+
+AUDIO=$1
+
+ffmpeg -v warning \
+  -f rawvideo -pixel_format rgb32 -framerate 30 -video_size "$RESOLUTION" -i pipe:0 \
+  -i "$AUDIO" -shortest \
+  -c:v rawvideo -pix_fmt rgb32 -c:a copy \
+  -f nut - \
+| ffplay -v warning -autoexit -
