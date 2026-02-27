b53a7084c6302c0561166c46d21332672cd014c4
Samuel Carlsson <samuel.carlsson@gmail.com>
2026-02-27 12:51:43 +0100

    clip to near plane instead of viewport

--- src/uv2xy.rs
+++ src/uv2xy.rs
@@ -1,15 +1,11 @@
-use nalgebra_glm::{distance, Vec2, Vec3};
+use nalgebra_glm::{distance, Vec3};
 
 use crate::{
     camera::Camera, eq::NewtonRaphsonOptions, geometry::Geometry, paper::ViewBox, polyline::Polyline2, raytracer::{backproject, Tracer}, sdf::SDF
 };
 
-fn contains(view_box: &ViewBox, point: &Vec2) -> bool {
-    let (x, y, w, h) = view_box;
-    point.x > *x as f32
-        && point.y > *y as f32
-        && point.x < (x + w) as f32
-        && point.y < (y + h) as f32
+fn in_front_of_camera(screen: &Vec3) -> bool {
+    screen.z > 0.0
 }
 
 // handle occlusions
@@ -26,13 +22,13 @@ fn visible(world: &Vec3, screen: &Vec3, camera: &Camera, geometry: &impl SDF, tr
 // Takes uv-coordinates and returns xy-cordinates
 // 1. Evaluates geometry
 // 2. Project using camera
-// 3. Clip to viewport
+// 3. Drop points behind the camera
 // 4. Handle occlusion
 pub fn reproject<G: Geometry + SDF>(
     polyline: &Polyline2,
     geometry: &G,
     camera: &Camera,
-    area: ViewBox,
+    _area: ViewBox,
     near: f32,
     far: f32,
 ) -> Vec<Polyline2> {
@@ -50,7 +46,7 @@ pub fn reproject<G: Geometry + SDF>(
     let mut polylines = Vec::new();
     let mut current = Polyline2::new();
     for (world, screen) in points {
-        if contains(&area, &screen.xy()) && visible(&world, &screen, &camera, geometry, &tracer) {
+        if in_front_of_camera(&screen) && visible(&world, &screen, &camera, geometry, &tracer) {
             current.add(screen.xy());
         } else {
             polylines.push(current);
